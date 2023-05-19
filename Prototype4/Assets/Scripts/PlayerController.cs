2cd9388d9b505ef0a2d99b296fb97562bc59e7de
Chase <chase.hyska@gmail.com>
2023-05-19 13:39:19 -0400

    unit 4.3

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -13,11 +13,43 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
     }
-
+    public GameObject powerupIndicator;
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    public bool hasPowerup;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private float powerupStrength = 15.0f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to  " + hasPowerup);
+
+        Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+        enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
     }
 }
